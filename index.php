7ae0afdbcb5f15f1953bffe7b9ecd9d8cc5345c1
JoseMSalas <josemsalastrejo@gmail.com>
2025-03-14 10:52:03 -0600

    actualizacion2

--- index.php
+++ index.php
@@ -1,46 +1,61 @@
 <?php 
 
-if ($_GET) {
-	print_r($_GET);
-	die();
+$errores = '';
+
+if (isset($_POST['submit'])) {
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+
+	# Si no esta vacio el nombre entonces comprobamos que sea una cadena de texto limpia.
+	if (!empty($nombre)) {
+
+		// $nombre = trim($nombre);
+		// $nombre = htmlspecialchars($nombre);
+		// $nombre = stripslashes($nombre);
+
+
+		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+		echo 'Tu nombre es: ' . $nombre . '<br>';
+	} else {
+		$errores .= 'Por favor ingresa un nombre <br />';
+	}
+
+	if (!empty($correo)) {
+		$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+
+		if (!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+			$errores .= 'Por favor ingresa un correo valido <br />';
+		} else {
+			echo 'Tu correo es: ' . $correo;
+		}
+
+	} else {
+		$errores .= 'Por favor ingresa un correo <br />';
+	}
+
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Formulario</title>
+	<title>Document</title>
+	
+	<style>
+		.error {color:red;}
+	</style>
 </head>
 <body>
-	<form action="recibe.php" name="formulario_contacto" method="">
-		<input type="text" placeholder="Nombre:" name="nombre" id="nombre">
-		<input type="text" placeholder="Edad:" name="edad" id="edad">
-		<br>
+	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+		<input type="text" name="nombre" placeholder="Nombre">
+		<input type="text" name="correo" placeholder="Correo">
 
-<!-- Radio Button -->
-		<label for="hombre">Hombre</label>
-		<input type="radio" name="sexo" id="hombre" value="hombre">
-		<br>
+		<?php if(!empty($errores)): ?>
+			<div class="error"><?php echo $errores; ?></div>
+		<?php endif; ?>
 
-		<label for="mujer">Mujer</label>
-		<input type="radio" name="sexo" id="mujer" value="mujer">
-		<br>
-
-<!-- Combobox -->
-		<select name="year" id="year">
-			<option value="2000">2000</option>
-			<option value="2001">2001</option>
-		</select>
-		<br>
-
-<!-- Checkbox -->
-		<label for="terminos">Terminos y Condiciones</label>
-		<input type="checkbox" name="terminos" value="ok" id="terminos">
-		<br>
-
-		<input type="submit" name="btn-enviar" vale="Enviar">
+		<input type="submit" name="submit">
 	</form>
 </body>
 </html>
